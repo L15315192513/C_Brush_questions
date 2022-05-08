b0fdb2dd1ac17ee6b9d4b1c9698387964fd0e571
L15315192513 <15315192513@189.cn>
2022-05-08 18:19:58 +0800

    BC5 牛牛学说话之-字符

---
+++ Nowcoder/2022_05_08__17_52/main.c
@@ -0,0 +1,27 @@
+/*
+* 描述:
+* 会说浮点数之后，牛牛开始尝试字符
+* 输入一个字符，输出这个字符。
+* 输入描述：
+* 输入一个字符，范围在ascii范围内
+* 输出描述：
+* 输出这个字符
+* 示例1
+* 输入：
+* a
+* 输出：
+* a
+*/
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	char i;
+
+	i = getchar();
+
+	putchar(i);
+
+	return 0;
+}
