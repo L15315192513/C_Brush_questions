b56dc9cf2b21eac990b865216514069f19feced8
L15315192513 <15315192513@189.cn>
2022-05-08 17:06:09 +0800

    BC1 Hello Nowcoder

---
+++ Nowcoder/2022_05_07__23_07/main.c
@@ -0,0 +1,17 @@
+/*
+* 描述:
+* 输出"Hello Nowcoder!"。开始你的编程之旅吧。
+* 输入描述：
+* 无
+* 输出描述：
+* Hello Nowcoder!
+*/
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello Nowcoder!\n");
+
+	return 0;
+}
